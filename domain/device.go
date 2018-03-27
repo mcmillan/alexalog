30d0d92547fc588f96457e0028a58a2c0eff7f59
Josh McMillan <josh@joshmcmillan.co.uk>
2018-03-27 03:49:33 +0100

    Have SupportedInterfaces prop on Device be a map rather than array

--- domain/device.go
+++ domain/device.go
@@ -1,6 +1,6 @@
 package domain
 
 type Device struct {
-	DeviceID            string   `json:"deviceId"`
-	SupportedInterfaces []string `json:"supportedInterfaces"`
+	DeviceID            string                 `json:"deviceId"`
+	SupportedInterfaces map[string]interface{} `json:"supportedInterfaces"`
 }
