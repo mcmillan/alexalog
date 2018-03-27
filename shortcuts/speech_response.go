58e80aa39b3d6d3ec658ce20e91da980076618ec
Josh McMillan <josh@joshmcmillan.co.uk>
2018-03-27 06:02:08 +0100

    Improve SpeechResponse shortcut

--- shortcuts/speech_response.go
+++ shortcuts/speech_response.go
@@ -2,15 +2,11 @@ package shortcuts
 
 import "github.com/mcmillan/alexalog/domain"
 
-func SpeechResponse(text string, endSession bool) domain.ResponseBody {
-	return domain.ResponseBody{
-		Version: "1.0",
-		Response: &domain.Response{
-			OutputSpeech: &domain.ResponseOutputSpeech{
-				Type: "PlainText",
-				Text: text,
-			},
-			ShouldEndSession: endSession,
+func SpeechResponse(text string) *domain.Response {
+	return &domain.Response{
+		OutputSpeech: &domain.ResponseOutputSpeech{
+			Type: "PlainText",
+			Text: text,
 		},
 	}
 }
